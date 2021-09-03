5dae6fa5c7df097ac00ae74bab4f2f0b86fbb76e
Ben <Fuzzinator12@gmail.com>
2021-09-02 20:27:43 -0400

    Started scoringmanager

---
+++ Assets/Scripts/Scoring/ScoringManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ScoringManager : MonoBehaviour
+{
+    public static ScoringManager Instance;
+
+    public int CurrentScore
+    {
+        get => _currentScore;
+        private set
+        {
+            var oldScore = _currentScore;
+            _currentScore = value;
+            if (oldScore != _currentScore)
+            {
+                currentScoreUpdated?.Invoke(_currentScore);
+            }
+        }
+    }
+
+    private int _currentScore;
+    
+    public UnityEvent<int> currentScoreUpdated = new UnityEvent<int>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        CurrentScore = 0;
+    }
+
+    public void AddToScore(int valueToAdd)
+    {
+        CurrentScore += valueToAdd;
+    }
+}
