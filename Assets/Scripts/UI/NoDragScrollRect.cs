d7f118c0d07bd522c25a6cf25009464e117797bf
Ben <Fuzzinator12@gmail.com>
2022-01-03 01:11:20 -0500

    Disabled dragging and added scroll buttons

---
+++ Assets/Scripts/UI/NoDragScrollRect.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class NoDragScrollRect : ScrollRect
+{
+
+   public override void OnInitializePotentialDrag(PointerEventData eventData)
+   {
+   }
+
+   public override void OnBeginDrag(PointerEventData eventData)
+   {
+   }
+
+   public override void OnEndDrag(PointerEventData eventData)
+   {
+   }
+   
+   public override void OnDrag(PointerEventData eventData)
+   {
+   }
+}
