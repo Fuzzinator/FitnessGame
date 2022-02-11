591079e3382d492ccc4f26fba9b4f99a30badbe4
Ben <Fuzzinator12@gmail.com>
2022-02-10 23:41:36 -0500

    Garbage collector manually controlled now. No more dropping below 72 fps

--- Assets/Scripts/GameManagement/GarbageCollectorController.cs
+++ Assets/Scripts/GameManagement/GarbageCollectorController.cs
@@ -1,13 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Scripting;
 
 public class GarbageCollectorController : MonoBehaviour
 {
+    [SerializeField]
+    private int _frameCount = 10;
+
+    private CancellationToken _cancellationToken;
+    
     // Start is called before the first frame update
     void Start()
     {
+        _cancellationToken = this.GetCancellationTokenOnDestroy();
+        BackgroundGarbageCollectorAsync().Forget();
     }
 
     public void SetGarbageCollectorState(bool enabled)
@@ -19,5 +29,29 @@ public class GarbageCollectorController : MonoBehaviour
 
     public void RunGarbageCollector()
     {
+        RunGarbageCollectorAsync().Forget();
+    }
+
+    private async UniTaskVoid RunGarbageCollectorAsync()
+    {
+        for (var i = 0; i < _frameCount; i++)
+        {
+            await UniTask.DelayFrame(1, cancellationToken: _cancellationToken);
+            if (_cancellationToken.IsCancellationRequested)
+            {
+                return;
+            }
+
+            GarbageCollector.CollectIncremental(1000000 );
+        }
+    }
+
+    private async UniTaskVoid BackgroundGarbageCollectorAsync()
+    {
+        while (!_cancellationToken.IsCancellationRequested)
+        {
+            GarbageCollector.CollectIncremental(100000);
+            await UniTask.Delay(TimeSpan.FromSeconds(1f), cancellationToken:_cancellationToken);
+        }
     }
 }
