d5da91abeb0821ff3ef6dd7aac00395a59f32bc8
Ben <Fuzzinator12@gmail.com>
2023-01-07 17:50:01 -0500

    updated logging and error reporting. Fixed hook offsets

--- Assets/Scripts/Testing/ErrorReporter.cs
+++ Assets/Scripts/Testing/ErrorReporter.cs
@@ -9,15 +9,19 @@ public class ErrorReporter : MonoBehaviour
 
     [SerializeField]
     private Canvas _debugCanvas;
-    
+
     public static ErrorReporter Instance { get; private set; }
 
     private Application.LogCallback _callback;
 
+    private ES3Settings _settings;
+
+    private const string ERRORLOG = "Log.dat";
+
     private const string ERRORTITLE = "An error has occurred.";
 
     private const string ERRORBODY =
-        "A restart may be required. This is a Beta build and some of the kinks haven't been worked out yet.";
+        "An error has occured that may affect interactions with ShadowBoXR. If you run into issues restarting the game may be required.";
 
     private void Awake()
     {
@@ -33,15 +37,17 @@ public class ErrorReporter : MonoBehaviour
 
     private void Start()
     {
+        _settings = new ES3Settings(ERRORLOG);
         _callback = (text, stacktrace, logType) =>
-                    {
-                        if (logType != LogType.Error)
-                        {
-                            //Log(text);
-                            return;
-                        }
-                        DisplayNotification(text, stacktrace);
-                    };
+        {
+            LogMessage(text, stacktrace, logType);
+            if (logType != LogType.Error)
+            {
+                return;
+            }
+
+            DisplayNotification(text, stacktrace);
+        };
         Application.logMessageReceived += _callback;
     }
 
@@ -50,13 +56,18 @@ public class ErrorReporter : MonoBehaviour
         Application.logMessageReceived -= _callback;
     }
 
+    private void LogMessage(string text, string stacktrace, LogType logType)
+    {
+        ES3.Save(DateTime.Now.ToString("yyyyMMddHHmmss"), $"{logType}: {text}\n{stacktrace}", _settings);
+    }
+
     private void DisplayNotification(string text, string stacktrace)
     {
         var visuals =
-            new Notification.NotificationVisuals(ERRORBODY, ERRORTITLE, "Details", autoTimeOutTime: 5f, popUp: true);
+            new Notification.NotificationVisuals(ERRORBODY, ERRORTITLE, autoTimeOutTime: 5f, popUp: true);
         NotificationManager.RequestNotification(visuals, () => Log($"{text}\n{stacktrace}"));
     }
-    
+
     public static void Log(string text)
     {
         Instance._debugCanvas.gameObject.SetActive(true);
